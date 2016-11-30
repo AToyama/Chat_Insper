df202fa8313406b9b300011d6143c7f8474c56a1
AToyama <toyama_andre@hotmail.com>
2016-11-30 15:01:33 -0200

    server update/Chat client

--- server.js
+++ server.js
@@ -9,8 +9,27 @@ var socketIO = require("socket.io");
  var httpServer = http.createServer(app);
  var io = socketIO.listen(httpServer);    
 
+//array contando os sockets conectados
+connections = [];
+
 //definindo a porta
 httpServer.listen(process.env.PORT || 8080);
 
 //printa no prompt se o servidor estiver online
 console.log('Online');
+
+//acessando a pasta onde estarao os arquivos do "chat client"
+app.use(express.static((__dirname, 'client')));
+
+
+io.sockets.on('connection',function(socket){
+	//Connect
+	connections.push(socket);
+	console.log('Connected: %s users connected: ', connections.length);
+
+	//Disconnect
+	socket.on('disconnect', function(data){
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Disconnected: %s users connected', connections.length);
+	});
+});
